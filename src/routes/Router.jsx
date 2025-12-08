8a7842b00a4e7da8d9bf6c95192746313a82af8f
Md Hasibur Rahman <hasib2920@hotmail.com>
2025-12-08 22:04:34 +0600

    map set-up using react leaflet

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Rootlayout from "../layouts/Rootlayout";
 import Home from "../pages/Home/Home/Home";
 import ErrorPage from "../pages/error/ErrorPage";
+import Coverage from "../pages/coverage/Coverage";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,11 @@ export const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+      {
+        path: "/coverage",
+        loader: () => fetch("/serviceCenters.json").then((res) => res.json()),
+        Component: Coverage,
       },
     ],
   },
