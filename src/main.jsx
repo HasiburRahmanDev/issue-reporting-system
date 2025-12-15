08d34885cdd71672732601e7e9887aafe11f3529
Md Hasibur Rahman <hasib2920@hotmail.com>
2025-12-15 13:19:29 +0600

    used tanstack to fetch data

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes/Router.jsx";
 import AuthProvider from "./contexts/AuthContext/AuthProvider.jsx";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-      <ToastContainer />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <ToastContainer />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/pages/Dashboard/MyIssues.jsx
+++ src/pages/Dashboard/MyIssues.jsx
@@ -1,9 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import UseAuth from "../../hooks/UseAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const MyIssues = () => {
+  const { user } = UseAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: issues = [] } = useQuery({
+    queryKey: ["myIssues", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/issues?email=${user.email}`);
+      return res.data;
+    },
+  });
   return (
     <div>
-      <h1>All of my issues</h1>
+      <h1>All of my issues: {issues.length}</h1>
     </div>
   );
 };
